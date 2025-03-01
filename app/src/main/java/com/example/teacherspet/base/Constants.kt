47d6e0cc8de01a90bfac502858ba4dde4c57e818
Ofri Yaakov <ofriyaakov@gmail.com>
2025-03-01 18:00:37 +0200

    add user model

---
+++ app/src/main/java/com/example/teacherspet/base/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.teacherspet.base
+
+typealias EmptyCallback = () -> Unit
---
+++ app/src/main/java/com/example/teacherspet/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.teacherspet.model
+
+data class User (
+    var name: String,
+    var grade: String,
+    var profession: String,
+    var email: String,
+    var password: String
+)
