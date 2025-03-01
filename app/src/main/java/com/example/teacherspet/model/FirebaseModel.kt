305e7a87b96a64087ceab3ab0a37c3d2f95cd047
Ofri Yaakov <ofriyaakov@gmail.com>
2025-03-01 17:42:49 +0200

    add firebase model

---
+++ app/src/main/java/com/example/teacherspet/model/FirebaseModel.kt
@@ -0,0 +1,19 @@
+package com.example.teacherspet.model
+
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.memoryCacheSettings
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class FirebaseModel {
+
+    private val database = Firebase.firestore
+
+    init {
+        val settings = firestoreSettings {
+            setLocalCacheSettings(memoryCacheSettings {  })
+        }
+        database.firestoreSettings = settings
+    }
+}
