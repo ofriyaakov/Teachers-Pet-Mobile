478472407a5f617864d4d0e26257fae335f0106d
Ofri Yaakov <ofriyaakov@gmail.com>
2025-03-12 22:25:23 +0200

    pull dev

--- app/src/main/java/com/example/teacherspet/LandingPageActivity.kt
+++ app/src/main/java/com/example/teacherspet/LandingPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.teacherspet
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -31,7 +32,7 @@ class LandingPage : AppCompatActivity() {
     }
 
     private fun toSignIn(){
-//        startActivity(Intent(this, SignInActivity::class.java))
+        startActivity(Intent(this, SignInActivity::class.java))
     }
 
     private fun toLogIn(){
