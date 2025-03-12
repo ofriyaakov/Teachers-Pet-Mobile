3586df72e5a012cac3deadbf3a1f7866a7606211
Ofri Yaakov <ofriyaakov@gmail.com>
2025-03-12 22:08:25 +0200

    fix cr notes

--- app/src/main/java/com/example/teacherspet/LandingPageActivity.kt
+++ app/src/main/java/com/example/teacherspet/LandingPageActivity.kt
@@ -11,7 +11,7 @@ class LandingPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.landing_page)
+        setContentView(R.layout.activity_landing_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
