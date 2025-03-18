249d6ed4df2f431d31a9a24c614ba94a84d7f738
Ofri Yaakov <ofriyaakov@gmail.com>
2025-03-19 01:41:35 +0200

    add post to firebase model

--- app/src/main/java/com/example/teacherspet/model/FirebaseModel.kt
+++ app/src/main/java/com/example/teacherspet/model/FirebaseModel.kt
@@ -1,22 +1,22 @@
-package com.example.teacherspet.model
-
+import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
 import com.example.teacherspet.base.Constants
 import com.example.teacherspet.base.EmptyCallback
-import com.example.teacherspet.base.UsersCallback
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestoreSettings
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.memoryCacheSettings
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.tasks.await
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.io.File
+import com.example.teacherspet.model.User
+import com.example.teacherspet.model.Post
 
 class FirebaseModel {
-
     private val database = Firebase.firestore
-
+    private val storage = Firebase.storage
     init {
         val settings = firestoreSettings {
             setLocalCacheSettings(memoryCacheSettings {  })
@@ -24,21 +24,58 @@ class FirebaseModel {
         database.firestoreSettings = settings
     }
 
-    fun add(user: User, callback: EmptyCallback) {
-        Log.d("onSavedClicked - 04", user.json.toString())
+    fun addUser(user: User, callback: EmptyCallback) {
         database.collection(Constants.Collections.USERS).document(user.id).set(user.json)
             .addOnCompleteListener {
-                Log.d("onSavedClicked - addOnCompleteListener", user.json.toString())
                 callback()
             }
             .addOnFailureListener {
                 Log.d("TAG", it.toString() + it.message)
-                Log.d("onSavedClicked - 06", it.toString() + it.message)
             }
-        Log.d("onSavedClicked - 05", "${ database.collection(Constants.Collections.USERS).get() }")
     }
 
-    fun getUser(id: String): Task<DocumentSnapshot> {
-        return database.collection(Constants.Collections.USERS).document(id).get()
+    fun addPost(post: Post, callback: EmptyCallback) {
+
+        val storageRef: StorageReference = storage.reference
+
+        val file = Uri.fromFile(File(post.imageUri))
+
+        val riversRef = storageRef.child("gs://teacher-s-pet-6c42a.firebasestorage.app/images")
+
+        val uploadTask = riversRef.putFile(file)
+
+        val mountainsRef = storageRef.child("file.jpg")
+
+        if (file !== null) {
+
+            uploadTask.addOnFailureListener {
+            }.addOnSuccessListener { taskSnapshot ->
+                taskSnapshot.metadata?.reference?.downloadUrl?.addOnSuccessListener { uri ->
+                }
+            }
+
+            database.collection(Constants.Collections.POSTS).document(post.id).set(post.json)
+                .addOnCompleteListener {
+                    callback()
+                }
+                .addOnFailureListener {
+                    Log.d("TAG", it.toString() + it.message)
+                }
+        }
+    }
+    fun uploadImage(image: Bitmap, name: String, callback: (String?) -> Unit) {
+        val storageRef = storage.reference
+        val imageRef = storageRef.child("images/$name.jpg")
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        var uploadTask = imageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            callback(null)
+        }.addOnSuccessListener { taskSnapshot ->
+            imageRef.downloadUrl.addOnSuccessListener { uri ->
+                callback(uri.toString())
+            }
+        }
     }
 }
